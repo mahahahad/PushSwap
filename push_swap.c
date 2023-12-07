489f4966df86886355736c5002dc4de71dd6ed3f
mahahahad <mahad.abd2003@gmail.com>
2023-12-07 19:19:56 +0400

    Add parameter support

--- push_swap.c
+++ push_swap.c
@@ -12,10 +12,28 @@
 
 #include "push_swap.h"
 
+void  print_str(const char *str)
+{
+  int i;
+
+  i = 0;
+  while (str[i])
+    write(1, &str[i++], 1);
+}
+
+void  print_arr(const char **arr, int len)
+{
+  for (int i = 1; i < len; ++i) {
+    print_str(arr[i]);
+    print_str(" ");
+  }
+}
+
 int	main(int argc, char const *argv[])
 {
 	(void)argv;
 	if (argc == 1)
 		return (0);
+  print_arr(argv, argc);
 	return (0);
 }
