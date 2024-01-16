3faa349eafd4b5b3ce107eaa4cf31e58aed543b4
mahahahad <mahad.abd2003@gmail.com>
2024-01-16 19:33:11 +0400

    Add general swap function prototype

--- push_swap.h
+++ push_swap.h
@@ -30,6 +30,7 @@ typedef struct		s_list
 t_list	*rra(t_list *head);
 t_list	*sa(t_list *head);
 void	ra(t_list **head);
+void	swap(t_list **head);
 void	push(t_list **first_stack, t_list **second_stack);
 
 #endif
